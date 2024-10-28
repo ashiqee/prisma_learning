77ed1769f796ea075a5f85a18b7651247baf3ad9
ashiqee <ashiq.buet73@gmail.com>
2024-10-28 23:52:55 +0600

    paginations

---
+++ src/pagination.ts
@@ -0,0 +1,31 @@
+import { PrismaClient } from "@prisma/client";
+
+
+const prisma = new PrismaClient();
+
+
+const  paginationSorting = async()=>{
+    //offset data
+    const offsetData = await prisma.post.findMany({
+        skip:2,
+        take:2
+    })
+
+    // console.log({offsetData});
+
+    //cursor based pagination
+    const cursorData = await prisma.post.findMany({
+        skip:2,
+        take:2,
+        cursor:{
+            id:4
+        }
+    })
+
+    console.log({cursorData});
+    
+}
+
+
+
+paginationSorting()
